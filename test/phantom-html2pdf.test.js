bd49cd72e780ccaab3e680cc20e4ad6e9fc40dbd
Samuel Rossille <samuel.rossille@gmail.com>
2016-04-03 16:05:20 +0200

    Add basic non - regression test for the most frequent usecase
    Changes highlights:
    
    package.json
    - Add devDependencies: mocha (test runner) and istanbul (to measure code
    coverage)
    - Add npm "test" lifecycle script: now "npm test" runs the mocha tests,
    wrapped with istanbul to measure coverage. Test coverage is not amazing
    so far, but that's a start :=)
    
        Statements   : 46.21% ( 61/132 )
        Branches     : 49.3% ( 35/71 )
        Functions    : 53.57% ( 15/28 )
        Lines        : 49.18% ( 60/122 )
    
    test/phantom-html2pdf.test.js: The test file for phantom-html2pdf.js
    (todo: add tests for other files)
    
    .gitignore: add the coverage files and the data directory of my favorite
    IDE

---
+++ test/phantom-html2pdf.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var assert = require('assert');
+
+var pdf = require('../lib/phantom-html2pdf');
+
+describe('phantom-html2pdf.js', function() {
+  describe('Simple end to end test', function() {
+
+    it('Should generate a PDF file without crashing', function(done) {
+      var pdfOptions = {
+        'html': '<!DOCTYPE html><html lang="en"><body><h1>Hello</h1><p>World</p></body></html>',
+        'papersize': {format: 'A4', orientation: 'portrait', border: '1cm'}
+      };
+
+      pdf.convert(pdfOptions, function (result) {
+        result.toBuffer(function (buffer) {
+          assert(buffer, 'A buffer is returned');
+          assert(buffer.length > 0, 'The generated buffer is not empty');
+          done();
+        });
+      });
+    });
+  })
+});
